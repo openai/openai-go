673e457205b0fb2e0716f0433491d7a1f9886f4f
springrain <chunanyong@163.com>
2025-02-01 23:35:31 +0800

    Update Request URL

--- internal/requestconfig/requestconfig.go
+++ internal/requestconfig/requestconfig.go
@@ -305,7 +305,8 @@ func (cfg *RequestConfig) Execute() (err error) {
 		return fmt.Errorf("requestconfig: base url is not set")
 	}
 
-	cfg.Request.URL, err = cfg.BaseURL.Parse(strings.TrimLeft(cfg.Request.URL.String(), "/"))
+	// cfg.Request.URL, err = cfg.BaseURL.Parse(strings.TrimLeft(cfg.Request.URL.String(), "/"))
+	cfg.Request.URL, err = url.Parse(cfg.BaseURL.String() + "/" + cfg.Request.URL.Path)
 	if err != nil {
 		return err
 	}
