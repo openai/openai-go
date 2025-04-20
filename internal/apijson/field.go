38f161a37dce83b014122d1ca33cdc94485bab2c
Nikita Sivukhin <sivukhin@turso.tech>
2025-04-20 15:31:16 +0400

    handle missing case properly

--- internal/apijson/field.go
+++ internal/apijson/field.go
@@ -14,7 +14,7 @@ type Field struct {
 	status status
 }
 
-// Returns true if the field is explicitly `null` _or_ if it is not present at all (ie, missing).
+// IsNull returns true if the field is explicitly `null` _or_ if it is not present at all (ie, missing).
 // To check if the field's key is present in the JSON with an explicit null value,
 // you must check `f.IsNull() && !f.IsMissing()`.
 func (j Field) IsNull() bool    { return j.status <= null }
