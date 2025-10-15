6e69a81662a8f94cbcaf66faee9b2ba240f66a00
ripark <ripark@microsoft.com>
2025-10-15 21:53:58 +0000

    Allow users to pass in a custom scope, if needed, when creating their OpenAI client.

--- azure/example_test.go
+++ azure/example_test.go
@@ -45,3 +45,29 @@ func Example_authentication() {
 		_ = client
 	}
 }
+
+func Example_authentication_custom_scopes() {
+	// Custom scopes can also be passed, if needed, when using Azure OpenAI endpoints.
+	const azureOpenAIEndpoint = "https://<your-azureopenai-instance>.openai.azure.com"
+	const azureOpenAIAPIVersion = "<api version string>"
+
+	// For a full list of credential types look at the documentation for the Azure Identity
+	// package: https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/azidentity
+	tokenCredential, err := azidentity.NewDefaultAzureCredential(nil)
+
+	if err != nil {
+		fmt.Printf("Failed to create TokenCredential: %s\n", err)
+		return
+	}
+
+	client := openai.NewClient(
+		azure.WithEndpoint(azureOpenAIEndpoint, azureOpenAIAPIVersion),
+		azure.WithTokenCredential(tokenCredential,
+			// This is an example of a custom scope. See documentation for your service
+			// endpoint for the proper value to pass.
+			azure.WithTokenCredentialScopes([]string{"your-custom-scope"}),
+		),
+	)
+
+	_ = client
+}
